8224aadc2dc7426c173f62f3769ebd1e8759aacc
Lassi Kortela <lassi@lassi.io>
2019-04-16 16:41:13 +0300

    Reformat using shfmt

--- build-macos.sh
+++ build-macos.sh
@@ -3,4 +3,4 @@ set -eux
 ${CC:-clang} -Wall -Wextra -pedantic -std=c99 -c shm_open_anon.c
 ${CC:-clang} -Wall -Wextra -pedantic -std=c99 -c test_shared.c
 ${CC:-clang} -Wall -Wextra -pedantic -std=c99 \
-             -o test_shared shm_open_anon.c test_shared.c
+	-o test_shared shm_open_anon.c test_shared.c
