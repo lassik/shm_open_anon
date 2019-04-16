b457510ee4dc78bc0277d252bcdd514056ed6638
Lassi Kortela <lassi@lassi.io>
2019-04-16 16:41:31 +0300

    Add OpenBSD build script

---
+++ build-openbsd.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -eux
+${CC:-clang} -Wall -Wextra -pedantic -std=c99 -c shm_open_anon.c
+${CC:-clang} -Wall -Wextra -pedantic -std=c99 -c test_shared.c
+${CC:-clang} -Wall -Wextra -pedantic -std=c99 \
+	-o test_shared shm_open_anon.c test_shared.c
