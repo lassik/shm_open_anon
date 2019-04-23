da0035eccc1965dd960096478e010fa177c2447b
Lassi Kortela <lassi@lassi.io>
2019-04-23 23:44:42 +0300

    Unify all build scripts into one
    Buddhist walks up to a hot dog stand...

--- build-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eux
-${CC:-gcc} -Wall -Wextra -pedantic -std=gnu99 -c shm_open_anon.c
-${CC:-gcc} -Wall -Wextra -pedantic -std=gnu99 -c test_shared.c
-${CC:-gcc} -Wall -Wextra -pedantic -std=gnu99 \
-	-o test_shared shm_open_anon.c test_shared.c -lrt
---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -eu
+case "$(uname)" in
+Darwin)
+	default_cc=clang
+	default_cflags="-Wall -Wextra -pedantic -std=c99"
+	default_lflags=""
+	;;
+DragonFly)
+	default_cc=gcc
+	default_cflags="-Wall -Wextra -pedantic -std=c99"
+	default_lflags=""
+	;;
+FreeBSD)
+	default_cc=clang
+	default_cflags="-Wall -Wextra -pedantic -std=c99"
+	default_lflags=""
+	;;
+Linux)
+	default_cc=gcc
+	default_cflags="-Wall -Wextra -pedantic -std=gnu99"
+	default_lflags="-lrt"
+	;;
+NetBSD)
+	default_cc=gcc
+	default_cflags="-Wall -Wextra -pedantic -std=c99"
+	default_lflags="-lrt"
+	;;
+OpenBSD)
+	default_cc=clang
+	default_cflags="-Wall -Wextra -pedantic -std=c99"
+	default_lflags=""
+	;;
+*)
+	echo "Operating system not supported: $(uname)" >&2
+	exit 1
+	;;
+esac
+CC="${CC:-$default_cc}"
+CFLAGS="${CFLAGS:-$default_cflags}"
+LFLAGS="${LFLAGS:-$default_lflags}"
+set -x
+$CC $CFLAGS -c shm_open_anon.c
+$CC $CFLAGS -c test_shared.c
+$CC $LFLAGS -o test_shared test_shared.o shm_open_anon.o
