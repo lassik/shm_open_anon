7a40b37dd3ac53631be7265fbb7a6be1fb1d127c
Lassi Kortela <lassi@lassi.io>
2019-04-26 16:22:20 +0300

    Use random characters instead of mktemp()

--- shm_open_anon.c
+++ shm_open_anon.c
@@ -13,6 +13,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #undef IMPL_MEMFD
@@ -32,29 +33,29 @@
 #endif
 
 #ifdef __HAIKU__
-#define IMPL_POSIX "/shmXXXXXXX"
+#define IMPL_POSIX "/shm-"
 #endif
 
 #ifdef __NetBSD__
-#define IMPL_POSIX "/shmXXXXXXX"
+#define IMPL_POSIX "/shm-"
 #endif
 
 #ifdef __APPLE__
 #ifdef __MACH__
-#define IMPL_POSIX "/shmXXXXXXX"
+#define IMPL_POSIX "/shm-"
 #endif
 #endif
 
 #ifdef __sun
-#define IMPL_POSIX "/shmXXXXXXX"
+#define IMPL_POSIX "/shm-"
 #endif
 
 #ifdef __DragonFly__
-#define IMPL_POSIX "/tmp/shmXXXXXXX"
+#define IMPL_POSIX "/tmp/shm-"
 #endif
 
 #ifdef __OpenBSD__
-#define IMPL_SHM_MKSTEMP "/shmXXXXXXX"
+#define IMPL_SHM_MKSTEMP "/shm-"
 #endif
 
 #ifdef IMPL_POSIX
@@ -85,11 +86,31 @@ shm_unlink_or_close(const char *name, int fd)
 int
 shm_open_anon(void)
 {
+	const char alphabet[] = "0123456789"
+	                        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	                        "abcdefghijklmnopqrstuvwxyz";
 	char name[16] = IMPL_POSIX;
+	char *p;
+	size_t n;
 	int fd;
 
-	if (mktemp(name) == NULL)
+	if ((fd = open("/dev/random", O_RDONLY)) == -1) {
 		return -1;
+	}
+	errno = 0;
+	p = strchr(name, 0);
+	n = sizeof(name) - 1 - (p - name);
+	if (read(fd, p, n) != (ssize_t)n) {
+		return -1;
+	}
+	if (close(fd) == -1) {
+		return -1;
+	}
+	while (p < name + sizeof(name) - 1) {
+		*p = alphabet[*p % strlen(alphabet)];
+		p++;
+	}
+	*p = 0;
 	fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL | O_NOFOLLOW, 0600);
 	if (fd == -1)
 		return -1;
@@ -99,9 +120,9 @@ shm_open_anon(void)
 
 #ifdef IMPL_SHM_MKSTEMP
 int
-shm_open_anon(void)
+pshm_open_anon(void)
 {
-	char name[16] = IMPL_SHM_MKSTEMP;
+	char name[16] = IMPL_SHM_MKSTEMP "XXXXXXXXXX";
 	int fd;
 
 	if ((fd = shm_mkstemp(name)) == -1)
